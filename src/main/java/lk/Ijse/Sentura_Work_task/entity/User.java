014faf882d7c6dc063b465014ab980f96b45fac4
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-04-04 09:43:42 +0530

    connection establish

---
+++ src/main/java/lk/Ijse/Sentura_Work_task/entity/User.java
@@ -0,0 +1,19 @@
+package lk.Ijse.Sentura_Work_task.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class User {
+    @Id
+    private String id;
+    private String display_name;
+
+}
